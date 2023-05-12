40b1500df5f2fd4e6a9e0ade320d5cc81d8681ac
carlos <pachecoc010@gmail.com>
2023-05-12 11:32:33 -0400

    Primer componente

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Cart } from './componets/Cart'
+
+ export function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <Cart/>
+      </div>
+      
+    </>
+  )
+}
+
+
---
+++ vite-project/src/componets/Cart.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Cart.css"
+ export const Cart = () => {
+  return (
+    <div className='container'>
+        <span>Crunchy y butterscoth</span>
+        <p>Butterscotch y Cashews</p>
+        <p>18</p>
+        <button className='boton'>Add</button>
+
+        
+    </div>
+  )
+}
+
